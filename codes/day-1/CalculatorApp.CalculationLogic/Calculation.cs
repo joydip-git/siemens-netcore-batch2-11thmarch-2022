272e328d0c907ef11c8bf8475f2873a8e8181fa9
Joydip <joydip.mondal@gmail.com>
2024-03-11 16:50:26 +0530

    Calculator App update

---
+++ codes/day-1/CalculatorApp.CalculationLogic/Calculation.cs
@@ -0,0 +1,28 @@
+﻿namespace CalculatorApp.CalculationLogic
+{
+    public class Calculation
+    {
+        //a special method without any return type
+        //it is called during object creation
+        //it's job is to assign default or user values to data members of the class
+        //constructor name must be same as that of the class name        
+        //public Calculation()
+        //{
+            
+        //}
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        //expression body syntax of writing methods
+        //function signature => function body
+        public int Subtract(int x, int y) => x - y;
+
+        //public void Multiply(int x, int y) => Console.WriteLine(x * y);
+        public int Multiply(int x, int y) => (x * y);
+
+        public int Divide(int x, int y) => y != 0 ? x / y : throw new DivideByZeroException("divisor can not be zero");
+
+    }
+}
---
+++ codes/day-1/CalculatorApp.UserInterface/Program.cs
@@ -0,0 +1,103 @@
+﻿using CalculatorApp.CalculationLogic;
+
+char decision = 'n';
+try
+{
+    do
+    {
+        PrintChoices();
+        int choice = GetChoice();
+        int firstValue = GetValue();
+        int secondValue = GetValue();
+        Calculation calculationObjectReference = new Calculation();
+        int result = Calculate(calculationObjectReference, choice, firstValue, secondValue);
+        Console.WriteLine(result);
+
+        decision = ChangeDecision();
+    } while (decision != 'n');
+}
+catch (NullReferenceException ex)
+{
+    Console.WriteLine(ex);
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine(ex);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+#region local functions
+static void PrintChoices()
+{
+    Console.WriteLine("1. Add\n2. Subtract\n3. Multiply\n4. Divide");
+}
+static int GetChoice()
+{
+    Console.Write("Enter Choice[1/2/3/4]: ");
+    //Nullable reference type
+    //there is possibility that strChoice can store null
+    //hence declare the variable as nullable type
+    string? strChoice = Console.ReadLine(); //strChoice = null;
+    if (strChoice != null)
+    {
+        return int.Parse(strChoice);
+    }
+    else
+        throw new NullReferenceException("Null reference instead of proper choice");
+}
+static int GetValue()
+{
+    Console.Write("Enter Value: ");
+    string? strValue = Console.ReadLine();
+    if (strValue != null)
+    {
+        return int.Parse(strValue);
+    }
+    else
+        throw new NullReferenceException("Null reference instead of proper value entered");
+}
+static int Calculate(Calculation calculation, int choice, int first, int second)
+{
+    int result = 0;
+    switch (choice)
+    {
+        case 1:
+            result = calculation.Add(first, second);
+            break;
+
+        case 2:
+            result = calculation.Subtract(first, second);
+            break;
+
+        case 3:
+            result = calculation.Multiply(first, second);
+            break;
+
+        case 4:
+            result = calculation.Divide(first, second);
+            break;
+    }
+
+    return result;
+}
+
+static char ChangeDecision()
+{
+    Console.Write("enter n to terminate or anythong else to continue: ");
+    //null coalesce operator (??)
+    string strChoice = Console.ReadLine() ?? "";
+    if (strChoice != "")
+    {
+        char value = char.Parse(strChoice);
+        if (char.IsUpper(value))
+            return char.ToLower(value);
+
+        return value;
+    }
+    else
+        return 'n';
+}
+#endregion
