c20bfb8f77558a7767fcfa946b6c1bfd9c28c95a
Joydip <joydip.mondal@gmail.com>
2024-03-12 14:50:23 +0530

    account task upload

---
+++ codes/day-2/OnlineTransferApp/OnlineTransferApp.Entities/Account.cs
@@ -0,0 +1,79 @@
+﻿namespace OnlineTransferApp.Entities
+{
+    public class Account
+    {
+        #region Data Members
+        string? accountHolderName;
+        readonly int accountId;
+        decimal currentBalance;
+        readonly static decimal minimumBalance;
+        readonly static decimal withdrawalLimit;
+        #endregion
+
+        #region Constructors
+        static Account()
+        {
+            minimumBalance = 500;
+            withdrawalLimit = 15000;
+        }
+        public Account()
+        {
+
+        }
+        public Account(int accountId, string? accountHolderName, decimal currentBalance)
+        {
+            this.accountHolderName = accountHolderName;
+            this.accountId = accountId;
+            this.currentBalance = currentBalance;
+        }
+        #endregion
+
+        #region Properties
+        public int AccountId => accountId;
+        public static decimal MinimumBalance => minimumBalance;
+        public static decimal WithdrawalLimit => withdrawalLimit;
+
+        public string? AccountHolderName { get => accountHolderName; set => accountHolderName = value; }        
+        public decimal CurrentBalance { get => currentBalance; private set => currentBalance = value; }
+       
+        #endregion
+
+        #region Methods
+        public bool CreditAmount(decimal amount)
+        {
+            this.CurrentBalance += amount;
+            return true;
+        }
+        public bool DebitAmount(decimal amount)
+        {
+            if (amount > 0)
+            {
+                if (amount < currentBalance)
+                {
+                    if (amount < withdrawalLimit)
+                    {
+                        decimal temp = currentBalance - amount;
+                        if (temp < minimumBalance)
+                        {
+                            throw new Exception($"the minimum balance will not be maintained if this transfer happens");
+                        }
+                        else
+                        {
+                            this.currentBalance -= amount;
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception($"You have entered amount more than your withdrawal limit");
+                    }
+                }
+                else
+                    throw new Exception($"You have entered amount more than your current balance");
+            }
+            else
+                throw new Exception($"You have entered amount less than or equal to zero");
+        }
+        #endregion
+    }
+}
---
+++ codes/day-2/OnlineTransferApp/OnlineTransferApp.UserInterface/Program.cs
@@ -0,0 +1,51 @@
+﻿using OnlineTransferApp.Entities;
+
+namespace OnlineTransferApp.UserInterface
+{
+    internal class Program
+    {
+        static Account CreateAccount()
+        {            
+            Console.Write("enter name: ");
+            string name = Console.ReadLine() ?? throw new FormatException("Incorrect format");
+            Console.Write("enter id: ");
+            int accId = int.Parse(Console.ReadLine() ?? throw new FormatException("incorrect format"));
+            Console.Write("enter current balance: ");
+            decimal current = decimal.Parse(Console.ReadLine() ?? throw new FormatException("incorrect format"));
+
+            Account account = new Account(accId,name,current);
+            return account;
+        }
+        static decimal AskForTransferAmount()
+        {
+            Console.WriteLine("enter amount to transfer");
+            decimal amount = decimal.Parse(Console.ReadLine() ?? throw new FormatException("incorrect format"));
+            return amount;
+        }
+        static void Transfer(Account debitAcc, Account creditAcc)
+        {
+            decimal amount = AskForTransferAmount();
+            bool debitStatus = false;
+            debitStatus = debitAcc.DebitAmount(amount);
+            if (debitStatus)
+            {
+                creditAcc.CreditAmount(amount);
+                Console.WriteLine("amount is successfully transfered");
+            }
+        }
+        static void Main()
+        {
+            try
+            {
+                Account creditAccount = CreateAccount();
+                Account debitAccount = CreateAccount();
+                Transfer(debitAccount, creditAccount);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
